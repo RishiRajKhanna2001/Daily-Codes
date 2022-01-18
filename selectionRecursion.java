875a976fc6d7041163feb8b164314361043355df
Rishi Raj Khanna <gs2013070@sgsitsindore.in>
2022-01-18 02:39:20 -0800

    17 JAN

---
+++ selectionRecursion.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class selectionRecursion {
+    public static void main(String[] args) {
+        int[] arr={4,9,2,5};
+        selection(arr,arr.length,0,0);
+        System.out.print(Arrays.toString(arr));
+    }
+    static void selection(int[] arr,int r,int c,int max)
+    {
+       if(r==0)
+       {
+           return;
+       }
+
+       if(c<r)
+       {
+          if(arr[c]>arr[max])
+          {
+            selection(arr, r, c+1, c);
+          }
+          else{
+            selection(arr, r, c+1, max);
+          }
+       }
+        else{
+            int temp=arr[r-1];
+            arr[r-1]=arr[max];
+            arr[max]=temp;
+          selection(arr, r-1, 0, 0);
+       }
+    }
+}
